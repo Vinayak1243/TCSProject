f6696866434430cfbf1ee95dd906e0616931dfca
Vinayak1243 <61154754+Vinayak1243@users.noreply.github.com>
2020-06-14 17:35:51 +0530

    todays work

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ from flask import (
     render_template,
     request,
     session,
-    url_for
+    url_for,
+    flash
 )
+import sqlite3 as sql
 
 app = Flask(__name__)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
@@ -39,16 +41,40 @@ def before_request():
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
+
     if request.method == 'POST':
+
         session.pop('user_id', None)
 
         username = request.form['username']
         password = request.form['password']
-
-        user = [x for x in users if x.username == username][0]
+        try:
+            user = [x for x in users if x.username == username][0]
+        except:
+            flash('Invalid username or password', 'danger')
+            return redirect(url_for('login'))
         if user and user.password == password:
+            print("here")
             session['user_id'] = user.id
-            return redirect(url_for('profile'))
+            if "Customer_Account" in request.form:
+                con = sql.connect("bank.db")
+                print("user1")
+                cur = con.cursor()
+                cur.execute(
+                    "INSERT INTO AccountExecutive_Login (username,password) VALUES (?,?)", (username, password))
+                con.commit()
+                con.close()
+                flash('Login Successful', 'Danger')
+                return redirect(url_for('profile'))
+            elif "Cashier" in request.form:
+
+                con = sql.connect("bank.db")
+                cur = con.cursor()
+                cur.execute(
+                    "INSERT INTO Cashier_Login (username,password) VALUES (?,?)", (username, password))
+                con.commit()
+                con.close()
+                return redirect(url_for('profile2'))
 
         return redirect(url_for('login'))
 
@@ -62,12 +88,14 @@ def profile():
 
     return render_template('profile.html')
 
+
 @app.route('/create')
 def create():
     if not g.user:
         return redirect(url_for('login'))
     else:
         return render_template('create.html')
+
 
 @app.route('/account')
 def account():
@@ -76,12 +104,14 @@ def account():
     else:
         return render_template('account.html')
 
+
 @app.route('/delacc')
 def delacc():
     if not g.user:
         return redirect(url_for('login'))
     else:
         return render_template('delacc.html')
+
 
 @app.route('/update1')
 def update1():
@@ -90,12 +120,14 @@ def update1():
     else:
         return render_template('update1.html')
 
+
 @app.route('/delcust')
 def delcust():
     if not g.user:
         return redirect(url_for('login'))
     else:
         return render_template('delcust.html')
+
 
 @app.route('/update2')
 def update2():
@@ -104,12 +136,14 @@ def update2():
     else:
         return render_template('update2.html')
 
+
 @app.route('/custstatus')
 def custstatus():
     if not g.user:
         return redirect(url_for('login'))
     else:
         return render_template('custstatus.html')
+
 
 @app.route('/accstatus')
 def accstatus():
@@ -118,12 +152,23 @@ def accstatus():
     else:
         return render_template('accstatus.html')
 
+
 @app.route('/profile2')
 def profile2():
     if not g.user:
         return redirect(url_for('login'))
+
+    return render_template('profile2.html')
+
+
+@app.route('/update')
+def update():
+    if not g.user:
+        return redirect(url_for('login'))
     else:
-        return render_template('Profile2.html')
+        return render_template('update.html')
+
+
 @app.route('/getdetails')
 def getdetails():
     if not g.user:
@@ -131,4 +176,37 @@ def getdetails():
     else:
         return render_template('cash-getdetails.html')
 
-app.run(debug=True)    
+
+@app.route('/deposit')
+def deposit():
+    if not g.user:
+        return redirect(url_for('login'))
+    else:
+        return render_template('deposit.html')
+
+
+@app.route('/withdraw')
+def withdraw():
+    if not g.user:
+        return redirect(url_for('login'))
+    else:
+        return render_template('withdraw.html')
+
+
+@app.route('/transfer')
+def transfer():
+    if not g.user:
+        return redirect(url_for('login'))
+    else:
+        return render_template('transfer.html')
+
+
+@app.route('/statement')
+def statement():
+    if not g.user:
+        return redirect(url_for('login'))
+    else:
+        return render_template('statement.html')
+
+
+app.run(debug=True)
